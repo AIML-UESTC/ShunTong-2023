078a69acb70dbc37f47356bc00c9fd6ece1d5ace
furyhorn <742047848@qq.com>
2023-06-08 13:12:41 +0800

    add wangjing streamlit

--- dockerimagescript/startallimages.sh
+++ dockerimagescript/startallimages.sh
@@ -4,3 +4,4 @@ nohup docker run -p 8502:8502 webrtc8502 > logs/webrtc8502.out 2>&1 &
 nohup docker run -p 8503:8503 streamlitmainpage8503 > logs/streamlitmainpage8503.out 2>&1 &
 nohup docker run -p 8505:8505 wfccellseg8505 > logs/wfccellseg8505.out 2>&1 & 
 nohup docker run -p 8504:8504 hyx8504 > logs/hyx8504.out 2>&1 &
+nohup docker run -p 8506:8506 wj8506 > logs/wj8506.out 2>&1 &
---
+++ streamlitMainpage/pages/A-wangjing.py
@@ -0,0 +1,21 @@
+import streamlit as st
+import webbrowser
+import pandas as pd
+import numpy as np
+from PIL import Image
+st.subheader('项目名称 xxx项目')
+st.subheader('项目描述')
+st.text('该项目是xxxxx')
+st.subheader('项目展示')
+#st.subheader('点击链接跳转至yolov5的目标检测展示-streamlit cloud')
+#link = '[Visit yolov5的目标检测](https://133dzs-dongzheming-2023-main-hixde5.streamlit.app/)'
+#st.markdown(link, unsafe_allow_html=True)
+
+st.subheader('点击链接跳转至kmeans-本地docker')
+link = '[Visit kmeans](http://121.48.165.52:8506)'
+st.markdown(link, unsafe_allow_html=True)
+
+
+st.subheader('联系方式')
+st.text('github: ')
+st.text('email: ')
