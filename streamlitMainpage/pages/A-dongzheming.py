2b992faae44ca61ea827b6eab046a5e7ce3db1ad
furyhorn <742047848@qq.com>
2023-06-14 11:37:15 +0800

    deal conflict

--- streamlitMainpage/pages/A-dongzheming.py
+++ streamlitMainpage/pages/A-dongzheming.py
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 import streamlit as st
 import webbrowser
 import pandas as pd
@@ -20,26 +19,3 @@ st.markdown(link, unsafe_allow_html=True)
 st.subheader('联系方式')
 st.text('github: ')
 st.text('email: ')
-=======
-import streamlit as st
-import webbrowser
-import pandas as pd
-import numpy as np
-from PIL import Image
-st.subheader('项目名称 xxx项目')
-st.subheader('项目描述')
-st.text('该项目是xxxxx')
-st.subheader('项目展示')
-st.subheader('点击链接跳转至yolov5的目标检测展示-streamlit cloud')
-link = '[Visit yolov5的目标检测](https://133dzs-dongzheming-2023-main-hixde5.streamlit.app/)'
-st.markdown(link, unsafe_allow_html=True)
-
-st.subheader('点击链接跳转至yolov5的目标检测展示-本地docker')
-link = '[Visit yolov5的目标检测](http://121.48.165.52:8501)'
-st.markdown(link, unsafe_allow_html=True)
-
-
-st.subheader('联系方式')
-st.text('github: ')
-st.text('email: ')
->>>>>>> 983e6b3a7f30c2fcb8fc5acff9034dbb2958916a
---
+++ streamlitMainpage/pages/T-standardDemo.py
@@ -0,0 +1,8 @@
+import streamlit as st
+import webbrowser
+import pandas as pd
+import numpy as np
+import os
+from PIL import Image
+pathpwd = os.getcwd()
+#path=os.path.join(pathpwd,"pages/resources/moon.jpg")
